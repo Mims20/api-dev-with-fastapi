cea2cca1a3f7e3b33d8dc90c9910bd8120018623
mims20 <psterlings@gmail.com>
2023-05-24 20:54:20 -0400

    modified sqlalchemy database url

--- app/database.py
+++ app/database.py
@@ -1,11 +1,17 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from . config import settings
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}" \
-                          f"@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+from dotenv import load_dotenv
 
+# SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}" \
+#                           f"@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
